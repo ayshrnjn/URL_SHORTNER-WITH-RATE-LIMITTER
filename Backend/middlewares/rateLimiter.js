aa82bac01074707e3ba5ac3cc2ee8f646cb6651c
ayshrnjn <162000786+ayshrnjn@users.noreply.github.com>
2025-07-30 18:45:11 +0530

    feat

--- Backend/middlewares/rateLimiter.js
+++ Backend/middlewares/rateLimiter.js
@@ -3,7 +3,7 @@ const rateLimit = require('express-rate-limit');
 // Create a limiter that allows 100 requests per 15 minutes
 const limiter = rateLimit({
     windowMs: 1 * 60 * 1000, // 15 minutes
-    max: 10, // Limit each IP to 100 requests per windowMs
+    max: 4, // Limit each IP to 100 requests per windowMs
     message: 'Too many requests from this IP, please try again after 15 minutes',
     standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
     legacyHeaders: false, // Disable the `X-RateLimit-*` headers
